e63d55ee2da87eabd3a71bdeee45153156a8fc8c
hypad590 <63247020+hypad590@users.noreply.github.com>
2022-12-23 16:19:06 +0300

    Create files.py

---
+++ files.py
@@ -0,0 +1,16 @@
+import os 
+
+list_paths = []
+
+for adress,papka,file in os.walk('C:\\'):
+    for i in file:
+        full_path = os.path.join(adress, i)
+        list_paths.append(full_path)
+
+
+r = open('text.txt', 'w')
+
+for x in list_paths:
+    r.write(x + '\n')
+
+r.close()
